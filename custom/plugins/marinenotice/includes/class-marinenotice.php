ed607dcf59bdf02f4b6975a377868ecb1071b8d1
Austin <austin.goudge@psycle.com>
2016-04-04 23:14:11 +0100

    Added creation, editing and deletion of geo locations.

--- custom/plugins/marinenotice/includes/class-marinenotice.php
+++ custom/plugins/marinenotice/includes/class-marinenotice.php
@@ -30,16 +30,14 @@ class MarineNotice {
 
         add_action('init', array($this, 'init'));
         add_action('wp_enqueue_scripts', array($this, 'addScriptsAndStyles'));
+        add_action('add_meta_boxes', array($this, 'addMetaBoxes'));
+        add_action('save_post', array($this, 'saveLocations'));
 
         add_shortcode('navionics', array($this, 'navionicsMapShortcode'));
     }
 
     /**
-     * Hook for 'the_posts' filter - detect whether we are on an OSX library item or category page and manufacture
-     * a post if so.
-     *
-     * @param array $posts The array of posts to filter
-     * @return array Filtered posts array (always either the passed-in array or an array with a single OSX post in it)
+     * Initialisation. Set up custom post type and taxonomies.
      */
     function init() {
         register_post_type( 'notice',
@@ -50,6 +48,12 @@ class MarineNotice {
                 ),
                 'public' => true,
                 'has_archive' => true,
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'thumbnail',
+                    'revisions'
+                )
             )
         );
 
@@ -75,23 +79,123 @@ class MarineNotice {
 		);
     }
 
+    /**
+     * Shortcode for displaying a Navionics Map
+     * @param type $attrs
+     * @return string
+     */
     function navionicsMapShortcode($attrs)
     {
-        return "<div class='test_map_div'></div>\n" .
-                "<script>\n" .
-                "var webapi = new JNC.Views.BoatingNavionicsMap({\n" .
-                "tagId: '.test_map_div',\n" .
-                "center: [  12.0, 46.0 ],\n" .
-                "navKey: 'Navionics_webapi_01136'\n" .
-                "});\n" .
-                "webapi.showSonarControl(false);\n" .
-                "</script>\n";
+        return "<div class='test_map_div'></div>
+                <script>
+                    var webapi = new JNC.Views.BoatingNavionicsMap({
+                        tagId: '.test_map_div',
+                        center: [  12.0, 46.0 ],
+                        zoom: 0,
+                        /* locale: 'CA', */
+                        ZoomControl: true,
+                        DistanceControl: false,
+                        SonarControl: false,
+                        LayerControl: false,
+                        navKey: 'Navionics_webapi_01136'
+                    });
+                    /*webapi.showSonarControl(true);*/
+                    webapi.loadKml('/?feed=geo_tag_kml', false);
+                </script>";
     }
 
+    /**
+     * Add all extra JS and CSS to the page
+     */
     function addScriptsAndStyles() {
         wp_register_script('navionicsJS', 'http://webapiv2.navionics.com/dist/webapi/webapi.min.no-dep.js', array(), false, false);
         wp_register_style('navionicsCSS', 'http://webapiv2.navionics.com/dist/webapi/webapi.min.css', array(), false, 'all');
         wp_enqueue_script('navionicsJS');
         wp_enqueue_style('navionicsCSS');
+    }
+
+    function addMetaBoxes($output) {
+		add_meta_box('marinenotice-locations', 'Locations', array($this, 'generateLocationsMetabox'), 'notice', 'normal', 'high');
+	}
+
+	function generateLocationsMetabox() {
+        echo "<form>";
+        wp_nonce_field('marinenotice-locations', 'marinenotice-locations');
+
+        $postMeta = get_post_meta(filter_input(INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT), "marinenotice-locations");
+        if ($postMeta != "") {
+            $locations = unserialize($postMeta[0]);
+		}
+
+		echo "<strong>Add Location</strong>
+            <p>
+                <label for='marinenotice-location-lat-0'>Latitude</label> <input name='marinenotice-location-lat-0' type='text' />
+                <label for='marinenotice-location-long-0'>Longitude</label> <input name='marinenotice-location-long-0' type='text' />
+            </p>";
+
+		if ($locations != "") {
+            echo "<strong>Existing Locations</strong>";
+            $index = 0;
+			foreach($locations as $location) {
+                if ($location['lat'] != "" && $location['long'] != "") {
+					$index++;
+					echo "<p>
+                            <label for='marinenotice-location-lat-" . $index . "'>Latitude</label> <input name='marinenotice-location-lat-" . $index . "' type='text' value='" . $location['lat'] . "' />
+                            <label for='marinenotice-location-long-" . $index . "'>Longitude</label> <input name='marinenotice-location-long-" . $index . "' type='text' value='" . $location['long'] . "' />
+                            <label for='marinenotice-location-delete-" . $index . "'>Delete</label> <input name='marinenotice-location-delete-" . $index . "' type='checkbox' />
+                        </p>";
+				}
+			}
+		}
+
+        echo "</form>";
+	}
+
+    function saveLocations($post_id) {
+        error_log("1");
+		if (!isset($_POST['marinenotice-locations']) || !wp_verify_nonce($_POST['marinenotice-locations'], 'marinenotice-locations')) {
+    		return $post_id;
+  		}
+
+         error_log("2");
+        if (!current_user_can('edit_post', $post_id)) {
+            return $post_id;
+        }
+
+		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+			return $post_id;
+        }
+
+		$locations = array();
+
+  		foreach($_POST as $key => $value) {
+            if (strpos($key, 'marinenotice-location') === FALSE) {
+                continue;
+            }
+
+			$parts = explode("-", $key);
+
+			if (!isset($parts[3])) {
+                continue;
+            }
+
+            $id = $parts[3];
+
+            switch ($parts[2]) {
+                case 'lat':
+                    $locations[$id]['lat'] = $value;
+                    break;
+                case 'long':
+                    $locations[$id]['long'] = $value;
+                    break;
+                case 'delete':
+                    $locations[$id] = false;
+                    break;
+                default:
+                    continue;
+            }
+		}
+
+		update_post_meta($post_id, "marinenotice-locations", serialize($locations));
     }
 }
