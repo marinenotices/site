4a863b72dfac8a2411ddf1e2f66b2b7863ba99cb
Austin <austin.goudge@psycle.com>
2016-04-05 19:49:20 +0100

    Added harbourmaster role and capabilities.

--- custom/plugins/marinenotice/includes/class-marinenotice.php
+++ custom/plugins/marinenotice/includes/class-marinenotice.php
@@ -60,7 +60,11 @@ class MarineNotice {
                     'editor',
                     'thumbnail',
                     'revisions'
-                )
+                ),
+                'show_ui' => true,
+                'show_in_admin_bar' => true,
+                'capability_type' => array('notice', 'notices'),
+                'map_meta_cap' => true,
             )
         );
 
@@ -84,6 +88,34 @@ class MarineNotice {
 				)
 			)
 		);
+
+// Once stabilised, get rid of remove_role and move the rest into a register_activation_hook()
+remove_role('harbourmaster');
+add_role('harbourmaster', "Harbourmaster", array(
+                                                        'read' => false,
+                                                        'edit_posts' => false,
+                                                        'delete_posts' => false,
+                                                        'publish_posts' => false,
+                                                        'upload_files' => true,
+                                                        ) );
+
+$roles = array('harbourmaster', 'editor', 'administrator');
+
+// Loop through each role and assign capabilities
+foreach($roles as $the_role) {
+    $role = get_role($the_role);
+
+    $role->add_cap( 'read_notice');
+    $role->add_cap( 'read_private_notices' );
+    $role->add_cap( 'edit_notice' );
+    $role->add_cap( 'edit_notices' );
+    $role->add_cap( 'edit_others_notices' );
+    $role->add_cap( 'edit_published_notices' );
+    $role->add_cap( 'publish_notices' );
+    $role->add_cap( 'delete_others_notices' );
+    $role->add_cap( 'delete_private_notices' );
+    $role->add_cap( 'delete_published_notices' );
+}
     }
 
     /**
